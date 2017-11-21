d95b2d2163be2ffef83559e74e40e584bd094686
fab <fdev31@gmail.com>
2017-11-21 08:38:04 +0100

    additions to js code

--- bugrest/code.js
+++ bugrest/code.js
@@ -1,3 +1,23 @@
+// compute content index
+document.querySelector('.document').childNodes.forEach( (e, i) => {if(e.id == 'contents') CONTENT_INDEX=i})
+
+window.run_filter = function() {
+    let name = document.querySelector('#filter_name').value;
+    let value = document.querySelector('#filter_value').value;
+    filterElements(name, value);
+}
+window.filterElements = function(name, value) {
+    let sections = document.querySelectorAll('.section');
+    name = name + ":"
+    let fields = new Array(... sections).map(e=> new Array(... e.querySelectorAll('tr.field')).filter(e=>!!e && e.querySelector('.field-name').textContent == name)[0])
+    fields = fields.filter( e => !!e && e.querySelector('.field-body').textContent == value)
+    let elements = fields.map(e => e.parentElement.parentElement.parentElement)
+    let html = elements.map(e => e.outerHTML).join('')
+    elements.forEach( e => e.outerHTML = '')
+    let header = new Array(... document.querySelector('.document').childNodes).slice(0, CONTENT_INDEX).map(e => e.outerHTML).join('')
+    document.querySelector('.document').innerHTML = header + html
+}
+
 window.sortElements = function(name) {
     let sections = document.querySelectorAll('.section');
     name = name + ":"
@@ -14,7 +34,7 @@ window.sortElements = function(name) {
     let elements = fields.map(e => e.parentElement.parentElement.parentElement)
     let ordered_html = elements.map(e => e.outerHTML).join('')
     elements.forEach( e => e.outerHTML = '')
-    let header = new Array(... document.querySelector('.document').childNodes).slice(0, 7).map(e => e.outerHTML).join('')
+    let header = new Array(... document.querySelector('.document').childNodes).slice(0, CONTENT_INDEX).map(e => e.outerHTML).join('')
     document.querySelector('.document').innerHTML = header + ordered_html
 }
 
