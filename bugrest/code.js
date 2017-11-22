f498d018e6c87c085ece6277de5d647110e0c757
fab <fdev31@gmail.com>
2017-11-22 09:15:00 +0100

    refactor js code

--- bugrest/code.js
+++ bugrest/code.js
@@ -1,5 +1,5 @@
 // compute content index
-document.querySelector('.document').childNodes.forEach( (e, i) => {if(e.id == 'contents') CONTENT_INDEX=i})
+document.querySelector('.document').childNodes.forEach( (e, i) => {if(e.id == 'header_limit') CONTENT_INDEX=i+1})
 
 window.run_filter = function() {
     let name = document.querySelector('#filter_name').value;
@@ -8,14 +8,10 @@ window.run_filter = function() {
 }
 window.filterElements = function(name, value) {
     let sections = document.querySelectorAll('.section');
-    name = name + ":"
     let fields = new Array(... sections).map(e=> new Array(... e.querySelectorAll('tr.field')).filter(e=>!!e && e.querySelector('.field-name').textContent == name)[0])
-    fields = fields.filter( e => !!e && e.querySelector('.field-body').textContent == value)
-    let elements = fields.map(e => e.parentElement.parentElement.parentElement)
-    let html = elements.map(e => e.outerHTML).join('')
-    elements.forEach( e => e.outerHTML = '')
-    let header = new Array(... document.querySelector('.document').childNodes).slice(0, CONTENT_INDEX).map(e => e.outerHTML).join('')
-    document.querySelector('.document').innerHTML = header + html
+    fields.forEach( e=> {
+        e.parentNode.parentNode.parentNode.style.display = (e.querySelector('.field-body').textContent == value)?'block':'none'
+    })
 }
 
 window.sortElements = function(name) {
@@ -38,4 +34,17 @@ window.sortElements = function(name) {
     document.querySelector('.document').innerHTML = header + ordered_html
 }
 
-// TODO: add a sorter dropdown on page load
+document.addEventListener("DOMContentLoaded", (e) => {
+    let options = [];
+    let opts_parsed = new Set();
+    for (let elt of document.querySelectorAll('.section')) {
+        for (let e of elt.querySelectorAll('.field-name')) {
+            opts_parsed.add(e.textContent);
+        }
+    }
+    opts_parsed.forEach((e)=> {
+        options.push(`<option value="${e}">${e}</option>`)
+    })
+    document.querySelector('#filter_name').innerHTML = options.join('\n');
+    document.querySelectorAll('.filter_input').forEach(e=> (addEventListener('keydown', (e)=> (e.keyCode == 13 && run_filter()))));
+})
