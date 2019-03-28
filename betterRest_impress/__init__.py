db7aa4b5fae2d1206bad6f0994fe8012f80580a9
fdev31 <fdev31@gmail.com>
2019-03-28 20:46:53 +0100

    add duration support

--- betterRest_impress/__init__.py
+++ betterRest_impress/__init__.py
@@ -18,11 +18,23 @@ KEEP_TRANSFORMS = True
 # - rot_reset
 
 SLIDE_TEMPLATE = '''<div class="step %(classes)s" data-rel-x="%(x)s" data-rel-y="%(y)s" data-rel-z="%(z)s"
-    data-rotate-x="%(rot_x)s" data-rotate-y="%(rot_y)s" data-rotate-z="%(rot_z)s" data-scale="%(scale)s"
->
+data-rotate-x="%(rot_x)s" data-rotate-y="%(rot_y)s" data-rotate-z="%(rot_z)s" data-scale="%(scale)s"
+data-transition-duration="%(duration)s" >
     %(title)s
         %(html)s
 </div>'''
+
+CUSTOM_CSS = '''
+.substep { opacity: 0; }
+.substep.substep-visible { opacity: 1; transition: opacity 1s; }
+.admonition {
+    border: solid 1px #333;
+    padding: 0 1em;
+}
+.admonition > .admonition-title {
+    font-weight: bold;
+}
+'''
 
 class CFG:
 
@@ -38,6 +50,8 @@ def get_resource(fname):
 
 def cmd_impress(handler):
     "impress: generate an HTML slideshow (on stdout)"
+
+    slide_duration = handler.info['duration'] or 1000
 
     text = ["""<html lang="en">
 <head>
@@ -56,13 +70,7 @@ def cmd_impress(handler):
 
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/styles/default.min.css">
     <style>
-    .admonition {
-        border: solid 1px #333;
-        padding: 0 1em;
-    }
-    .admonition > .admonition-title {
-        font-weight: bold;
-    }
+    %(custom_css)s
     </style>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/highlight.min.js"></script>
 
@@ -75,15 +83,17 @@ def cmd_impress(handler):
     <p>Your browser <b>doesn't support the features required</b> by impress.js, so you are presented with a simplified version of this presentation.</p>
     <p>For the best experience please use the latest <b>Chrome</b>, <b>Safari</b> or <b>Firefox</b> browser.</p>
 </div>
-<div id="impress" data-autoplay="%(autoplay)s">
+<div id="impress" data-autoplay="%(autoplay)s" data-transition-duration="%(duration)s">
         <div class="step" id="title" data-x="0" data-y="0">
         <h1>%(html_title)s</h1>
         </div>
     """%{
         'autoplay': handler.info['autoplay'] or 0,
+        'duration': slide_duration,
         'title': handler.info.title,
         'html_title': handler.info.title_as_html(),
         'css': get_resource('impress.css'),
+        'custom_css': CUSTOM_CSS,
         'mermaid': get_resource('mermaid.css'),
         }]
 
@@ -124,6 +134,7 @@ def cmd_impress(handler):
         props['z'] = bug['z'] or 0
         props['scale'] = bug['scale'] or 1
         props['classes'] = bug['class']
+        props['duration'] = bug['duration'] or slide_duration
 
         text.append(SLIDE_TEMPLATE%props)
 
